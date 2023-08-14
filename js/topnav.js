ec73d3e86e0ac6b2d11d360f28e0f9ab0507f137
PietroSko <99359354+PietroSko@users.noreply.github.com>
2023-08-14 16:50:39 +0000

    Atualizado

--- js/topnav.js
+++ js/topnav.js
@@ -6,3 +6,21 @@ function myFunction() {
       x.className = "topnav";
     }
   }
+
+
+
+  function adjustFontSize() {
+    const button = document.querySelector('.pf-button');
+    const buttonText = document.getElementById('button-text');
+
+    let fontSize = 16; // Tamanho de fonte inicial (pode ser ajustado)
+    const padding = 10; // Espaçamento interno para evitar que o texto toque nas bordas
+
+    while (buttonText.scrollWidth > button.offsetWidth - 2 * padding) {
+        fontSize--;
+        buttonText.style.fontSize = fontSize + 'px';
+    }
+}
+
+adjustFontSize();
+window.addEventListener('resize', adjustFontSize);
